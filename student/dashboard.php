72cb0c42b4e23ef08c3f355ba362f4136032339d
belimhamza227 <belimhamza227@gmail.com>
2026-07-19 10:55:29 +0530

    add student dashboard page

---
+++ student/dashboard.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+    <html lang="en">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+    content="width=device-width
+    initial-scal=1.0">
+
+    <title>
+        student Dashboard - KDP
+    </title>
+
+    <link rel="stylesheet" href="../
+    assets/css/student.css">
+</head>
+
+<body>
+<div class="dashboard">
+
+<img src="../assets/images/KDP-logo.png" alt="KDP Logo">
+
+     <h1> Welcome to K.D.polytechnic</h1>
+
+      <p>Digital Lab Manual & Expense tracker </p>
+</div>
+
+
+</body>
+
+</html>
