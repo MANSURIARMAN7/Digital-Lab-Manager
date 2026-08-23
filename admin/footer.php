3495719ad78b223d482ccd6e332741a631e7f9b2
sohanshaikh <sohanshaikh771@gmail.com>
2026-08-23 12:23:14 +0530

    footer

--- admin/footer.php
+++ admin/footer.php
@@ -1,7 +1,8 @@
         <!-- Dynamic Content Ends Here -->
-    </div> <!-- End Main Content -->
-    
-    <!-- Bootstrap JS Bundle (includes Popper) -->
+        </div><!-- End .content-area -->
+    </div><!-- End .main -->
+
+    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
