ef19dfbda62b8b3c615de7d9a4a492aa31ca5a07
sohanshaikh <sohanshaikh771@gmail.com>
2026-08-30 22:32:10 +0530

    admin/Submissions.php

--- admin/Submissions.php
+++ admin/Submissions.php
@@ -3,7 +3,7 @@ session_start();
 include '../db.php';
 
 // 1. Admin Login Check
-//  =====
+//  ======
 if (!isset($_SESSION['logged_in']) || $_SESSION['role'] !== 'admin') {
     header("Location: ../login.php");
     exit();
